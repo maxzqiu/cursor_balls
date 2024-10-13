5ce3655387bde39fd6cb5d8a0534ce89fe8bd185
Max Qiu <maxzqiu@gmail.com>
2024-10-13 11:13:39 -0700

    edit

--- src.js
+++ src.js
@@ -98,6 +98,19 @@ class Linear{
         return this.shape.radius>0;
     }
 }
+
+class Firework extends Linear{
+    constructor(shape){
+        super(shape);
+        let angle=Math.random()*1/4*Math.PI+3/8*Math.PI;
+        this.dir={x:9*Math.cos(angle),y:-9*Math.sin(angle)};
+    }
+}
+class FireworkBall extends Firework{
+    constructor(x,y,radius){
+        super(new Ball(x,y,radius))
+    }
+}
 class Bouncing{
     constructor(shape){
         this.shape=shape;
@@ -128,6 +141,23 @@ class Bouncing{
         return this.shape.radius>0;
     }
 }
+
+class FireworkParticle extends Bouncing{
+    constructor(shape){
+        super(shape);
+        let angle=Math.random()*2*Math.PI;
+        let speed=Math.random()*4+1
+        this.vel={x:speed*Math.cos(angle),y:speed*Math.sin(angle)};
+
+    }
+}
+
+class FireworkParticleBall extends FireworkParticle{
+    constructor(x,y,radius){
+        super(new Ball(x,y,radius));
+    }
+}
+
 class BouncingBall extends Bouncing{
     constructor(x,y,radius){
         super(new Ball(x,y,radius));
@@ -191,68 +221,6 @@ class RandomWalkSquare extends RandomWalk{
 
 
 
-// class Ball{
-//     constructor(x,y,radius){
-//         
-//         this.color=colors[Math.floor(Math.random()*colors.length)]
-//         this.radius=radius
-//         this.pos={x:x,y:y}
-//         this.transparency=1
-//         this.randomizeDirection();
-//         this.id=setInterval(()=>this.randomizeDirection(),500)
-//         this.angle=2
-        
-        
-
-//     }
-//     randomizeDirection(){
-//         
-//         this.dir={x:random_dir[0],y:random_dir[1]}
-//     }
-    
-//     
-
-// draw(ctx){
-//     const ANGLE=Math.PI/this.angle;
-    
-//     ctx.beginPath()
-            
-//     ctx.save();
-//     ctx.translate(this.pos.x,this.pos.y+10);
-//     ctx.rotate(ANGLE);
-            
-//     ctx.rect(0,0,this.radius,this.radius);
-//     ctx.stroke();
-//     ctx.restore();
-    
-//     ctx.shadowColor="black";
-//     ctx.shadowBlur=15;
-//     ctx.shadowOffsetX=10;
-//     ctx.shadowOffsetY=10;
-//     ctx.strokeStyle=this.color;
-//     ctx.fillStyle=this.color;
-//     ctx.globalAlpha=this.transparency;
-//     ctx.fill();
-//     ctx.stroke();
-//     }
-
-    
-//     tick(dt){
-//         this.pos.x+=dt*this.dir.x/16;
-//         this.pos.y+=dt*this.dir.y/16;
-//         this.transparency-=0.01
-//         this.radius-=dt*0.05/16
-//         this.angle+=1/2
-//         if (this.angle===2){
-//             this.angle=0;
-//         }
-//         if (this.radius<=0){  // :)
-//             clearInterval(this.id)
-//         }
-//         return this.radius>0
-//     }
-// }
-
 
 
 function run(){
@@ -282,6 +250,12 @@ function run(){
                 let keep=obj.tick(dt);
                 if (keep){
                     kept.push(obj)
+                } else if (obj instanceof FireworkBall){
+                    for (let i=0;i<300;i+=1){
+                        let ball=new FireworkParticleBall(obj.shape.pos.x,obj.shape.pos.y,5)
+                        kept.push(ball)
+                    }
+                    
                 }
                 
             }
@@ -301,16 +275,25 @@ function run(){
 
 
 
-    canvas.addEventListener("mousemove",(e)=>{
+    // canvas.addEventListener("mousemove",(e)=>{
 
+    //     const rect=canvas.getBoundingClientRect()
+    //     let x=e.clientX-rect.left
+    //     let y=e.clientY-rect.top
+    //     let ball=new BouncingBall(x,y,10)
+    //     objects.push(ball)
+    //  //   ball.draw(ctx)
+    
+                
+    // })
+
+    canvas.addEventListener("click",(e)=>{
         const rect=canvas.getBoundingClientRect()
         let x=e.clientX-rect.left
         let y=e.clientY-rect.top
-        let ball=new BouncingBall(x,y,10)
+        let ball=new FireworkBall(x,y,10)
         objects.push(ball)
      //   ball.draw(ctx)
-    
-                
     })
 
 
